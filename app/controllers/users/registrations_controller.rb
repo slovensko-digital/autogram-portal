6f642756c24c33250f9a8307ca4d6fa24f6b39e6
Ahmed Al Hafoudh <alhafoudh@freevision.sk>
2025-10-10 20:56:22 +0200

    Enhance user registration and profile management UI with improved forms and error handling

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,23 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters
+
   def destroy
     raise NotImplementedError
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
+  end
+
+  def update_resource(resource, params)
+    params = params.except(:current_password, :password, :password_confirmation)
+
+    resource.update(params)
+  end
+
+  def after_update_path_for(resource)
+    edit_user_registration_path
   end
 end
