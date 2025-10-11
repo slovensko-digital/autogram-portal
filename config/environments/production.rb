40be45b890e8acd4687b938f5ce8e912329b8fe9
Ahmed Al Hafoudh <alhafoudh@freevision.sk>
2025-10-11 12:41:58 +0200

    Enable email delivery in production by setting perform_deliveries to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: ENV.fetch("APP_HOST", "example.com"), protocol: "https" }
 
   # Configure SMTP email delivery
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: ENV.fetch("SMTP_HOST"),
