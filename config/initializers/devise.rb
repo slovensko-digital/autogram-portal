fb5b0f753d6db159bc591aeb63bcf7124ca19ad2
Ahmed Al Hafoudh <alhafoudh@freevision.sk>
2025-10-11 11:11:22 +0200

    Fix rubocop offences

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = ENV.fetch('MAIL_FROM', 'please-change-me-at-config-initializers-devise@example.com')
+  config.mailer_sender = ENV.fetch("MAIL_FROM", "please-change-me-at-config-initializers-devise@example.com")
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
@@ -36,7 +36,7 @@ Devise.setup do |config|
   # Load and configure the ORM. Supports :active_record (default) and
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
-  require 'devise/orm/active_record'
+  require "devise/orm/active_record"
 
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
@@ -58,12 +58,12 @@ Devise.setup do |config|
   # Configure which authentication keys should be case-insensitive.
   # These keys will be downcased upon creating or modifying a user and when used
   # to authenticate or find a user. Default is :email.
-  config.case_insensitive_keys = [:email]
+  config.case_insensitive_keys = [ :email ]
 
   # Configure which authentication keys should have whitespace stripped.
   # These keys will have whitespace before and after removed upon creating or
   # modifying a user and when used to authenticate or find a user. Default is :email.
-  config.strip_whitespace_keys = [:email]
+  config.strip_whitespace_keys = [ :email ]
 
   # Tell if authentication through request.params is enabled. True by default.
   # It can be set to an array that will enable params authentication only for the
@@ -97,7 +97,7 @@ Devise.setup do |config|
   # Notice that if you are skipping storage for all authentication paths, you
   # may want to disable generating routes to Devise's sessions controller by
   # passing skip: :sessions to `devise_for` in your config/routes.rb
-  config.skip_session_storage = [:http_auth]
+  config.skip_session_storage = [ :http_auth ]
 
   # By default, Devise cleans up the CSRF token on authentication to
   # avoid CSRF token fixation attacks. This means that, when using AJAX
@@ -316,10 +316,10 @@ Devise.setup do |config|
   # Need to use a custom Devise mailer in order to send magic links.
   # If you're already using a custom mailer just have it inherit from
   # Devise::Passwordless::Mailer instead of Devise::Mailer
-  config.mailer = 'Devise::Passwordless::Mailer'
+  config.mailer = "Devise::Passwordless::Mailer"
 
   # Which algorithm to use for tokenizing magic links. See README for descriptions
-  config.passwordless_tokenizer = 'SignedGlobalIDTokenizer'
+  config.passwordless_tokenizer = "SignedGlobalIDTokenizer"
 
   # Time period after a magic login link is sent out that it will be valid for.
   # config.passwordless_login_within = 20.minutes
