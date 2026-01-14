4c33da804baee2de3646a7ed5a4519184dd75c49
Marek Ceľuch <celuchmarek@gmail.com>
2026-01-14 13:14:14 +0100

    use user locales in devise mailer

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -1,4 +1,30 @@
 class DeviseMailer < Devise::Passwordless::Mailer
   layout "mailer"
   default from: ENV.fetch("MAIL_FROM", "noreply@example.com")
+
+  def magic_link(record, token, opts = {})
+    set_locale_for_user(record)
+    super
+  end
+
+  def confirmation_instructions(record, token, opts = {})
+    set_locale_for_user(record)
+    super
+  end
+
+  def email_changed(record, opts = {})
+    set_locale_for_user(record)
+    super
+  end
+
+  def unlock_instructions(record, token, opts = {})
+    set_locale_for_user(record)
+    super
+  end
+
+  private
+
+  def set_locale_for_user(user)
+    I18n.locale = user&.locale || I18n.default_locale
+  end
 end
